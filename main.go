76efce4b0c64ed5b2c479b58a4aa289e8fc8f525
dylens <ylsync@qq.com>
2015-06-14 13:11:32 +0800

    test

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	//net "code.google.com/p/go.net/websocket"
+	out "fmt"
+	"math"
+)
+
+type name string
+
+type (
+	字符串 string
+	s   字符串
+)
+
+func main() {
+	//godoc -http=:6060
+	var 玳 s
+	玳 = "小玳"
+	var y s = "yang"
+	dye := 玳 + y
+	out.Println(dye)
+	out.Println(math.MaxInt32)
+	var arr [10]int
+	arr[0] = 42
+	arr[1] = 12
+
+	a := [3]int{1, 2, 3}
+	b := [...]int{1, 2, 3}
+	c := [2][4]int{{1, 2, 3, 4}, {2, 3, 4, 5}}
+
+	slice := []int{2, 3, 5}
+
+}
